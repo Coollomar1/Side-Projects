6518cf1e2b924e235cd8c36336f2073a90ac3299
Coollomar1 <143557891+Coollomar1@users.noreply.github.com>
2024-10-11 20:49:27 -0400

    Update driver_main.c

--- Strands Game Program.c/driver_main.c
+++ Strands Game Program.c/driver_main.c
@@ -1,7 +1,6 @@
 /*
 Name: Omar Masri
 Date: 4/16/24
-CPSC 1070 AND 1071
 Project 1: Strands
 
 Code Description: driver_main.c file to test various cases. Ensures that this code runs efficiently and has no bugs
